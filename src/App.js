5da55d8093172323261e096c4e227d7c43c5a604
tugrankenger <turankenger@hotmail.com.tr>
2023-03-29 23:17:55 +0300

    create simple carts before using usecallback

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const phones = [
+  { name: 'xioami', price: 1000 },
+  { name: 'apple', price: 3000 },
+  { name: 'samsung', price: 2999 },
+];
+
 function App() {
+  const [products] = useState(phones);
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart((cart) => [...cart, product]);
+  };
+
+  const emptyCart = () => {
+    setCart([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <Products products={products} addToCart={addToCart} />
+      <Cart cart={cart} emptyCart={emptyCart} />
     </div>
   );
 }
+
+const Products = ({ products, addToCart }) => {
+  return (
+    <>
+      <h1>Products</h1>
+      <div className='products'>
+        {products.map((item) => {
+          return (
+            <Product
+              key={item.name}
+              name={item.name}
+              price={item.price}
+              addToCart={addToCart}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+const Product = ({ name, price, addToCart }) => {
+  return (
+    <>
+      <div className='product'>
+        <h2>{name}</h2>
+        <h3>{price.toLocaleString()} ₺</h3>
+        {addToCart && (
+          <button onClick={() => addToCart({name, price})}>Add to basket</button>
+        )}
+      </div>
+    </>
+  );
+};
+
+const Cart = ({ cart, emptyCart }) => {
+  return (
+    <>
+      <h1>
+        Basket : <button onClick={emptyCart}>Remove basket</button>
+      </h1>
+      <div className='products'>
+        {cart.map((item, i) => {
+          return <Product key={i} name={item.name} price={item.price} />;
+        })}
+      </div>
+    </>
+  );
+};
 
 export default App;
